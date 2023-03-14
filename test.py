0216f0e3495447238d160481c38416711f2063d0
Jaap Versteegh <j.r.versteegh@gmail.com>
2023-03-14 13:22:23 +0100

    Improve test output formatting

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ M = 6000
 def get_samples():
     """ Make a list of N samples chosen from [2, M]
     """
-    random.seed()
+    random.seed(11235)
     samples = random.sample(range(2, M+1), N)
     return sorted(samples)
 
@@ -35,11 +35,12 @@ if __name__ == '__main__':
     samples = get_samples()
     reference = pisano_example1.get_pisano_numbers(samples)
 
-    print("NAME\t\t\t\tRESULT\t  TIME\t  SIZE\t SCORE")
+    print(f"{'NAME':30s}{'RESULT':>8s}{'TIME':>8s}{'SIZE':>8s}{'SCORE':>8s}")
     for solution in [
             "pisano_example1",
+            "pisano_example2",
             "pisano",
     ]:
         result, runtime, file_size = do_test(solution, samples, reference)
         score = runtime + file_size
-        print(f"{solution:30s}\t{result}\t{runtime:6d}\t{file_size:6d}\t{score:6d}")
+        print(f"{solution:30s}{result:>8s}{runtime:8d}{file_size:8d}{score:8d}")
